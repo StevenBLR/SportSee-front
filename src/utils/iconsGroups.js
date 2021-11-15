4e9e364e4752015a41847d4c19d24cfe92732cd8
StevenBLR_PC <s.belair95@gmail.com>
2021-11-15 16:42:07 +0100

    Importing img files

---
+++ src/utils/iconsGroups.js
@@ -0,0 +1,11 @@
+import zen from "../imgs/zen.svg";
+import bicycle from "../imgs/bicycle.svg";
+import swim from "../imgs/swim.svg";
+import weight from "../imgs/weight.svg";
+
+export const sideBarBts = [
+  { icon: zen, name: "yoga" },
+  { icon: swim, name: "swiming" },
+  { icon: bicycle, name: "biking" },
+  { icon: weight, name: "workout" },
+];
