5ecebfc43fda4661e6c07c7d30bef02867b85626
StevenBLR_PC <s.belair95@gmail.com>
2021-11-23 00:10:04 +0100

    added responsive wrapper to piechart

--- src/components/graphs/PieGraph.js
+++ src/components/graphs/PieGraph.js
@@ -8,24 +8,29 @@ const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 function PieGraph() {
   return (
     <ChartWrapper>
-      <PieChart width="100% " height="100%">
-        <Pie
-          data={data}
-          cx={420}
-          cy={200}
-          startAngle={0}
-          endAngle={270}
-          innerRadius={60}
-          outerRadius={80}
-          fill="#8884d8"
-          paddingAngle={5}
-          dataKey="value"
-        >
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-          ))}
-        </Pie>
-      </PieChart>
+      <ResponsiveContainer width="100%" height="100%" className="pie-charts">
+        <PieChart width="100% " height="100%">
+          <Pie
+            data={data}
+            cx={420}
+            cy={200}
+            startAngle={0}
+            endAngle={270}
+            innerRadius={60}
+            outerRadius={80}
+            fill="#8884d8"
+            paddingAngle={5}
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
     </ChartWrapper>
   );
 }
